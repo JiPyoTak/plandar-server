735efd17d7115fd82497121264f09e606df49c9b
jintak0401 <jintak0401@naver.com>
2023-03-03 09:49:05 +0900

    feat: swagger 적용

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { HttpExceptionFilter } from '@/common/http-exception.filter';
 import { SuccessInterceptor } from '@/common/success.interceptor';
@@ -17,13 +18,21 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const PORT = configService.get('PORT');
   const CLIENT_URL = configService.get('CLIENT_URL');
-
   app.enableCors({
     origin: CLIENT_URL,
     credentials: true,
   });
 
-  await app.listen(PORT || 3000);
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Plandar API')
+    .setDescription('Plandar 서비스를 위한 API 문서')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT || 4000);
 }
 
 bootstrap();
