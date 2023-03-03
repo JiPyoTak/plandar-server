83fe63f84afc32b18565b8f9c39d358c2d2a10d1
jintak0401 <jintak0401@naver.com>
2023-03-03 16:22:45 +0900

    feat: RootRepository가 TypeORM의 Repository를 상속받도록 변경

--- src/common/root.repository.ts
+++ src/common/root.repository.ts
@@ -1,29 +1,18 @@
 import { Inject } from '@nestjs/common';
-import { EntityTarget, FindOneOptions, Repository } from 'typeorm';
+import { EntityTarget, Repository } from 'typeorm';
 
 import { TransactionManager } from '@/common/transaction.manager';
 
-export abstract class RootRepository<T> {
+export abstract class RootRepository<T> extends Repository<T> {
   constructor(
-    @Inject(TransactionManager) private readonly txManger: TransactionManager,
-  ) {}
+    @Inject(TransactionManager) private readonly txManager: TransactionManager,
+  ) {
+    super(RootRepository, txManager.getEntityManager());
+  }
 
   abstract getName(): EntityTarget<T>;
 
-  async save(t: T | T[]): Promise<void> {
-    await this.getRepo().save(Array.isArray(t) ? t : [t]);
-  }
-
-  async findById(id: number): Promise<T | null> {
-    const findOption: FindOneOptions = { where: { id } };
-    return this.getRepo().findOne(findOption);
-  }
-
-  async remove(t: T | T[]): Promise<void> {
-    await this.getRepo().remove(Array.isArray(t) ? t : [t]);
-  }
-
   protected getRepo(): Repository<T> {
-    return this.txManger.getEntityManager().getRepository(this.getName());
+    return this.txManager.getEntityManager().getRepository(this.getName());
   }
 }
