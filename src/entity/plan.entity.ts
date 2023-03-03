8170e03d388ee5b1b68cf4a431b95a0ea9e161a6
jintak0401 <jintak0401@naver.com>
2023-03-03 16:23:14 +0900

    feat: Plan Entity의 planName의 칼럼명을 'plan_name'으로 변경

--- src/entity/plan.entity.ts
+++ src/entity/plan.entity.ts
@@ -1,10 +1,15 @@
-import { Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToOne } from 'typeorm';
 
 import { DefaultEntity } from '@/entity/default.entity';
 import { User } from '@/entity/user.entity';
 
 @Entity('PLAN_TB')
 export class Plan extends DefaultEntity {
+  @Column({
+    name: 'plan_name',
+  })
+  planName: string;
+
   @ManyToOne(() => User, (user) => user.plans)
   user: User;
 }
