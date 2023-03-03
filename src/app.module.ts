08fe1f5792b9dbb512731787db80892c702bdb99
jintak0401 <jintak0401@naver.com>
2023-03-03 09:51:28 +0900

    feat: @Transactionl()로 트랜잭션이 되도록 기능 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,37 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from '@/api/user/user.module';
+import { TransactionMiddleware } from '@/common/middleware/transaction.middleware';
+import { Plan } from '@/entity/plan.entity';
+import { User } from '@/entity/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        port: 3306,
+        host: config.get('DB_HOST'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_SCHEMA'),
+        entities: [Plan, User],
+        // synchronize: config.get('NODE_ENV') === 'development', // true 시 테이블이 이미 존재하면 에러 발생
+        synchronize: false,
+        logging: config.get('NODE_ENV') === 'development',
+      }),
+    }),
+    UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TransactionMiddleware).forRoutes('*');
+  }
+}
