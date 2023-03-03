2f847dadd78ad81c6f385152001f43e9b0f06205
jintak0401 <jintak0401@naver.com>
2023-03-03 09:47:13 +0900

    refactor: 불필요한 코드들 제거

--- src/common/decorator/repository.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-import { TYPEORM_EX_CUSTOM_REPOSITORY } from '@/utils/constants';
-
-export function CustomRepository(entity): ClassDecorator {
-  return SetMetadata(TYPEORM_EX_CUSTOM_REPOSITORY, entity);
-}
