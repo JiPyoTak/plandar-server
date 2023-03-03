de207976ad2643c89febbcdcf23457a13f8df608
jintak0401 <jintak0401@naver.com>
2023-03-03 09:52:37 +0900

    feat: 각 Repository를 구현시 extends 해야할 GenericTypeOrmRepository 구현

---
+++ src/common/generic.repository.ts
@@ -0,0 +1,29 @@
+import { Inject } from '@nestjs/common';
+import { EntityTarget, FindOneOptions, Repository } from 'typeorm';
+
+import { TransactionManager } from '@/common/transaction.manager';
+
+export abstract class GenericTypeOrmRepository<T> {
+  constructor(
+    @Inject(TransactionManager) private readonly txManger: TransactionManager,
+  ) {}
+
+  abstract getName(): EntityTarget<T>;
+
+  async save(t: T | T[]): Promise<void> {
+    await this.getRepo().save(Array.isArray(t) ? t : [t]);
+  }
+
+  async findById(id: number): Promise<T | null> {
+    const findOption: FindOneOptions = { where: { id } };
+    return this.getRepo().findOne(findOption);
+  }
+
+  async remove(t: T | T[]): Promise<void> {
+    await this.getRepo().remove(Array.isArray(t) ? t : [t]);
+  }
+
+  protected getRepo(): Repository<T> {
+    return this.txManger.getEntityManager().getRepository(this.getName());
+  }
+}
