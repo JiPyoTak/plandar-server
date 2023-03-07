803ae45eecbed10b56e28cf8e48e318fd1b5100f
jintak0401 <jintak0401@naver.com>
2023-03-07 18:23:29 +0900

    feat: entity에 swagger 및 class-transformer/validator 적용 (#2)

--- src/entity/default.entity.ts
+++ src/entity/default.entity.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsPositive } from 'class-validator';
 import {
   CreateDateColumn,
   Entity,
@@ -7,12 +10,17 @@ import {
 
 @Entity()
 export class DefaultEntity {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
+  @IsPositive()
+  @Transform(({ value }) => Number(value))
   id: number;
 
+  @ApiProperty()
   @CreateDateColumn({ type: 'datetime', name: 'created_at' })
   createdAt: Date;
 
+  @ApiProperty()
   @UpdateDateColumn({ type: 'datetime', name: 'updated_at' })
   updatedAt: Date;
 }
--- src/entity/plan.entity.ts
+++ src/entity/plan.entity.ts
@@ -1,15 +1,27 @@
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 
 import { DefaultEntity } from '@/entity/default.entity';
 import { User } from '@/entity/user.entity';
 
 @Entity('PLAN_TB')
 export class Plan extends DefaultEntity {
+  @ApiProperty()
   @Column({
     name: 'plan_name',
+    type: 'varchar',
+    length: 30,
   })
+  @IsString()
+  @MaxLength(30)
+  @IsNotEmpty()
   planName: string;
 
-  @ManyToOne(() => User, (user) => user.plans)
+  @ApiProperty()
+  @ManyToOne(() => User, (user) => user.plans, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'user_id' })
   user: User;
 }
