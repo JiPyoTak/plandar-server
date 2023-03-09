66c42aec2d5e3369afa4bc5ca3e4272ea6a35cab
jintak0401 <jintak0401@naver.com>
2023-03-09 14:17:07 +0900

    feat: TypeORM 방식대로 entity를 imports 해주도록 변경 (#2)

--- src/api/plan/plan.module.ts
+++ src/api/plan/plan.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { TransactionManager } from '@/common/transaction.manager';
+import { Plan } from '@/entity/plan.entity';
 
 import { PlanController } from './plan.controller';
 import { PlanRepository } from './plan.repository';
 import { PlanService } from './plan.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Plan])],
   controllers: [PlanController],
-  providers: [PlanService, PlanRepository, TransactionManager],
+  providers: [PlanService, PlanRepository],
 })
 export class PlanModule {}
--- src/api/user/user.module.ts
+++ src/api/user/user.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { PlanRepository } from '@/api/plan/plan.repository';
 import { UserRepository } from '@/api/user/user.repository';
-import { TransactionManager } from '@/common/transaction.manager';
+import { Plan } from '@/entity/plan.entity';
+import { User } from '@/entity/user.entity';
 
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Plan])],
   controllers: [UserController],
-  providers: [UserService, UserRepository, PlanRepository, TransactionManager],
+  providers: [UserService, UserRepository, PlanRepository],
 })
 export class UserModule {}
