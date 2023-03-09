d55dd0d32ba1c1773a5e8442874d98100ea99d3a
jintak0401 <jintak0401@naver.com>
2023-03-09 14:10:49 +0900

    feat: @Transactional()을 사용하기 위한 설정 추가 (#2)

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { initializeTransactionalContext } from 'typeorm-transactional';
 
 import { HttpExceptionFilter } from '@/common/http-exception.filter';
 import { SuccessInterceptor } from '@/common/success.interceptor';
@@ -9,12 +10,18 @@ import { SuccessInterceptor } from '@/common/success.interceptor';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // transactional
+  initializeTransactionalContext();
+
+  const app = await NestFactory.create(AppModule, {
+    abortOnError: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new SuccessInterceptor());
   app.setGlobalPrefix('api');
 
+  // cors
   const configService = app.get(ConfigService);
   const PORT = configService.get('PORT');
   const CLIENT_URL = configService.get('CLIENT_URL');
