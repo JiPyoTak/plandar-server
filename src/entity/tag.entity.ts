a0086185a9784371e77a7e47fc597294e07ef610
seokho10007 <sukho1007@naver.com>
2023-03-16 17:36:00 +0900

    feat. Tag entity 생성 - name, user, plans column 생성 - User entity에 대한 1:N 관계 설정 - Plan entity에 대한 N:M 관계 설정

---
+++ src/entity/tag.entity.ts
@@ -0,0 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { Entity, Column, ManyToOne, JoinColumn, ManyToMany } from 'typeorm';
+
+import { DefaultEntity } from '@/entity/default.entity';
+
+import { Plan } from './plan.entity';
+import { User } from './user.entity';
+
+@Entity('tag_tb')
+export class Tag extends DefaultEntity {
+  @Column({ type: 'varchar', length: 20 })
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(20)
+  name: string;
+
+  @ApiProperty()
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'userId' })
+  user!: User;
+
+  @ApiProperty()
+  @ManyToMany(() => Plan, (plan) => plan.tags)
+  plans!: Plan[];
+}
