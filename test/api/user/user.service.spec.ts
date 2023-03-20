236be73fd025ecede680e48be5cad231cc82d6cf
seokho10007 <sukho1007@naver.com>
2023-03-20 16:06:27 +0900

    test: user service 테스트 코드 작성 (#21) - getUser, createUser 테스트 코드 작성

--- test/api/user/user.service.spec.ts
+++ test/api/user/user.service.spec.ts
@@ -1,10 +1,12 @@
-import { PlanRepository } from '@/api/plan/plan.repository';
 import { UserRepository } from '@/api/user/user.repository';
 import { UserService } from '@/api/user/user.service';
 import createTestingModule from 'test/utils/createTestingModule';
 
+import { USER_MOCK } from './mock';
+
 describe('UserService', () => {
-  let planRepository: PlanRepository;
+  const mockUser = Object.assign({}, USER_MOCK);
+
   let userRepository: UserRepository;
   let userService: UserService;
 
@@ -13,14 +15,48 @@ describe('UserService', () => {
       providers: [UserService],
     });
 
-    planRepository = moduleRef.get<PlanRepository>(PlanRepository);
     userRepository = moduleRef.get<UserRepository>(UserRepository);
     userService = moduleRef.get<UserService>(UserService);
   });
 
   it('Check defining Modules', () => {
-    expect(planRepository).toBeDefined();
     expect(userRepository).toBeDefined();
     expect(userService).toBeDefined();
+  });
+
+  describe('getUser', () => {
+    it('should return user', async () => {
+      // given
+      const id = mockUser.id;
+      const result = { ...mockUser };
+      const userRepoSpy = jest
+        .spyOn(userRepository, 'findOne')
+        .mockResolvedValue(result);
+
+      // when
+      const user = await userService.getUser(id);
+
+      // then
+      expect(userRepoSpy).toHaveBeenCalledWith(id);
+      expect(user).toEqual(result);
+    });
+  });
+
+  describe('createUser', () => {
+    it('should return new user', async () => {
+      // given
+      const newUser = { ...mockUser, id: undefined };
+      const result = { ...mockUser };
+      const userRepoSpy = jest
+        .spyOn(userRepository, 'createUser')
+        .mockResolvedValue(result);
+
+      // when
+      const user = await userService.createUser(newUser);
+
+      // then
+      expect(userRepoSpy).toHaveBeenCalledWith(newUser);
+      expect(user).toEqual(result);
+    });
   });
 });
