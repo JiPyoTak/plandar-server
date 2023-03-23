3e3dca75bc9d96f0171022c3f575228a540b9e84
jintak0401 <jintak0401@naver.com>
2023-03-23 14:06:57 +0900

    feat: Tag Service, Repository에서 사용할 인수 타입 추가 (#20)

--- src/dto/tag/tag.dto.ts
+++ src/dto/tag/tag.dto.ts
@@ -5,3 +5,14 @@ import { Tag } from '@/entity/tag.entity';
 export class TagReqDto extends PickType(Tag, ['name'] as const) {}
 export class TagDeleteReqDto extends PickType(Tag, ['id'] as const) {}
 export class TagResDto extends PickType(Tag, ['name', 'id'] as const) {}
+
+export interface CreateTagArgs {
+  userId: number;
+  tagName: string;
+}
+
+export interface UpdateTagArgs extends CreateTagArgs {
+  tagId: number;
+}
+
+export type DeleteTagArgs = CreateTagArgs;
