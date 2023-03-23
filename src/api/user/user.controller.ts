24312500ff51ac57f88f6f0d72d4b3472b559f0e
seokho10007 <sukho1007@naver.com>
2023-03-23 14:07:27 +0900

    feat: Swagger 관련 decorator 추가 (#19) (#25) - 각 에러 코드마다 앚맞은 데코레이터 추가

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,5 +1,11 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import {
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 import { UserService } from '@/api/user/user.service';
 import { User } from '@/decorators/user.decorator';
@@ -10,16 +16,18 @@ import { JwtAuthGuard } from '@/guard/jwt-auth.guard';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @ApiResponse({
-    status: 500,
+  @ApiInternalServerErrorResponse({
     description: '데이터 베이스에서 제대로 값을 읽어오지 못한 경우',
   })
-  @ApiResponse({ status: 400, description: '유저 정보가 존재하지 않을경우' })
+  @ApiUnauthorizedResponse({
+    description: 'access된 유저가 인증되지 않은 유저일 경우',
+  })
+  @ApiNotFoundResponse({ description: '유저 정보가 존재하지 않을경우' })
   @ApiResponse({ status: 200, type: UserEntity })
   @ApiOperation({ summary: '유저 정보 가져오기' })
   @UseGuards(JwtAuthGuard)
   @Get()
   getUser(@User() user: UserEntity) {
-    return this.userService.getUser(user.id);
+    return this.userService.getUser(user?.id);
   }
 }
