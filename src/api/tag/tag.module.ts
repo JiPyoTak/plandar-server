d4b809af112a1912b5695eb6b8f8c5b9dd14c544
jintak0401 <jintak0401@naver.com>
2023-03-23 15:42:28 +0900

    feat: Tag Module에 Tag Repository 추가 (#20)

--- src/api/tag/tag.module.ts
+++ src/api/tag/tag.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { TagRepository } from '@/api/tag/tag.repository';
+import { TypeOrmExModule } from '@/common/typeOrmEx.module';
+
 import { TagController } from './tag.controller';
 import { TagService } from './tag.service';
 
 @Module({
+  imports: [TypeOrmExModule.forFeature([TagRepository])],
   controllers: [TagController],
   providers: [TagService],
 })
