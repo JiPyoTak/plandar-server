98e027116116d819c8d165d811642f8e31e24da0
jintak0401 <jintak0401@naver.com>
2023-03-23 15:48:59 +0900

    feat: DeleteTagArgs 변경 (#20)

--- src/dto/tag/tag.dto.ts
+++ src/dto/tag/tag.dto.ts
@@ -3,7 +3,6 @@ import { PickType } from '@nestjs/swagger';
 import { Tag } from '@/entity/tag.entity';
 
 export class TagReqDto extends PickType(Tag, ['name'] as const) {}
-export class TagDeleteReqDto extends PickType(Tag, ['id'] as const) {}
 export class TagResDto extends PickType(Tag, ['name', 'id'] as const) {}
 
 export interface CreateTagArgs {
@@ -15,4 +14,7 @@ export interface UpdateTagArgs extends CreateTagArgs {
   tagId: number;
 }
 
-export type DeleteTagArgs = CreateTagArgs;
+export interface DeleteTagArgs {
+  userId: number;
+  tagId: number;
+}
