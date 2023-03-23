ebf442531e73e9d0750eb41e64df8077cd80e29b
seokho10007 <sukho1007@naver.com>
2023-03-23 22:30:15 +0900

    feat: TokenType 추가 (#29)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,8 @@
-export const PLANDAR_CUSTOM_REPSOITORY = 'PLANDAR_CUSTOM_REPSOITORY';
+const PLANDAR_CUSTOM_REPSOITORY = 'PLANDAR_CUSTOM_REPSOITORY';
+
+enum TokenType {
+  ACCESS = 'access',
+  REFRESH = 'refresh',
+}
+
+export { TokenType, PLANDAR_CUSTOM_REPSOITORY };
