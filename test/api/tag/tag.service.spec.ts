55129aa12271538b5f2f83f5bd67760448bf6a5a
jintak0401 <jintak0401@naver.com>
2023-03-23 16:26:21 +0900

    test: 실패 관련 테스트 코드 추가 (#17)

--- test/api/tag/tag.service.spec.ts
+++ test/api/tag/tag.service.spec.ts
@@ -1,3 +1,5 @@
+import { ConflictException } from '@nestjs/common';
+
 import { TagRepository } from '@/api/tag/tag.repository';
 import { TagService } from '@/api/tag/tag.service';
 import { STUB_TAG } from 'test/api/tag/stub';
@@ -23,6 +25,25 @@ describe('TagService', () => {
     expect(tagService).toBeDefined();
   });
 
+  describe('fail create tag', () => {
+    it('request create tag already existed', async () => {
+      // given
+      const tagName = stubTag.name;
+      const userId = stubTag.user.id;
+      const alreadyExistTag = { name: stubTag.name, id: stubTag.id };
+      const params = { userId, tagName };
+      jest.spyOn(tagRepo, 'findTagByName').mockResolvedValue(alreadyExistTag);
+
+      try {
+        // when
+        await tagService.createTag(params);
+      } catch (e) {
+        // then
+        expect(e).toBeInstanceOf(ConflictException);
+      }
+    });
+  });
+
   describe('success create tag', () => {
     it("should return created tag's name and id", async () => {
       // given
@@ -43,26 +64,88 @@ describe('TagService', () => {
     });
   });
 
-  describe('update tag', () => {
-    it("should return updated tag's name and id", async () => {
+  describe('fail update tag', () => {
+    it('change name with the same name', async () => {
       // given
       const userId = stubTag.user.id;
+      const tagId = stubTag.id;
+      const newTagName = stubTag.name;
+      const params = {
+        userId,
+        tagId,
+        tagName: newTagName,
+      };
+
+      jest
+        .spyOn(tagRepo, 'findTagById')
+        .mockResolvedValue({ id: tagId, name: newTagName });
+
+      try {
+        // when
+        await tagService.updateTag(params);
+      } catch (e) {
+        // then
+        expect(e).toBeInstanceOf(ConflictException);
+      }
+    });
+  });
+
+  describe('fail update tag', () => {
+    it('try to change name of not existed tag', async () => {
+      // given
+      const userId = stubTag.user.id;
+      const tagId = stubTag.id;
       const newTagName = 'new tag name';
       const params = {
         userId,
-        tagId: stubTag.id,
+        tagId,
+        tagName: newTagName,
+      };
+
+      jest.spyOn(tagRepo, 'findTagById').mockResolvedValue(null);
+
+      try {
+        // when
+        await tagService.updateTag(params);
+      } catch (e) {
+        // then
+        expect(e).toBeInstanceOf(ConflictException);
+      }
+    });
+  });
+
+  describe('success update tag', () => {
+    it("should return updated tag's name and id", async () => {
+      // given
+      const userId = stubTag.user.id;
+      const tagId = stubTag.id;
+      const newTagName = 'new ' + stubTag.name;
+      const params = {
+        userId,
+        tagId,
         tagName: newTagName,
       };
       const shouldBe = { name: newTagName, id: stubTag.id };
-      const tagRepoSpy = jest
+      const tagRepoUpdateTag = jest
         .spyOn(tagRepo, 'updateTag')
         .mockResolvedValue(shouldBe);
+
+      const tagRepoFindByTagId = jest
+        .spyOn(tagRepo, 'findTagById')
+        .mockResolvedValue({
+          id: tagId,
+          name: stubTag.name,
+        });
 
       // when
       const result = await tagService.updateTag(params);
 
       // then
-      expect(tagRepoSpy).toHaveBeenCalledWith(params);
+      expect(tagRepoUpdateTag).toHaveBeenCalledWith(params);
+      expect(tagRepoFindByTagId).toHaveBeenCalledWith({
+        userId,
+        tagId,
+      });
       expect(result).toEqual(shouldBe);
     });
   });
