f7d35f8948137327b38999b63123046991d0c5da
jintak0401 <jintak0401@naver.com>
2023-03-23 14:39:05 +0900

    feat: Tag Repository 함수 구현 (#20)

--- src/api/tag/tag.repository.ts
+++ src/api/tag/tag.repository.ts
@@ -1,7 +1,20 @@
 import { Repository } from 'typeorm';
 
 import { CustomRepository } from '@/common/customRepository.decorator';
+import { CreateTagArgs, DeleteTagArgs, UpdateTagArgs } from '@/dto/tag/tag.dto';
 import { Tag } from '@/entity/tag.entity';
 
 @CustomRepository(Tag)
-export class TagRepository extends Repository<Tag> {}
+export class TagRepository extends Repository<Tag> {
+  async createTag({ tagName, userId }: CreateTagArgs) {
+    return this.save(this.create({ name: tagName, user: { id: userId } }));
+  }
+
+  async updateTag({ tagName, userId, tagId }: UpdateTagArgs) {
+    return this.update({ id: tagId, user: { id: userId } }, { name: tagName });
+  }
+
+  async deleteTag({ tagName, userId }: DeleteTagArgs) {
+    return this.delete({ name: tagName, user: { id: userId } });
+  }
+}
