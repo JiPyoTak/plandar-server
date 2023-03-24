4a791e514c6d4ef75abc49e1e64a0efdb36d80f5
jintak0401 <jintak0401@naver.com>
2023-03-24 17:10:10 +0900

    test: Category Controller 테스트 코드 구현 (#31)

---
+++ test/api/category/category.controller.spec.ts
@@ -0,0 +1,200 @@
+import { INestApplication } from '@nestjs/common';
+import * as testRequest from 'supertest';
+
+import { CategoryController } from '@/api/category/category.controller';
+import { CategoryService } from '@/api/category/category.service';
+import createTestingModule from 'test/utils/createTestingModule';
+
+import { STUB_CATEGORY } from './stub';
+
+describe('CategoryController', () => {
+  const stubCategory = Object.assign({}, STUB_CATEGORY);
+  let app: INestApplication;
+  let categoryService: CategoryService;
+
+  beforeEach(async () => {
+    const moduleRef = await createTestingModule({
+      controllers: [CategoryController],
+    });
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+    categoryService = await app.resolve<CategoryService>(CategoryService);
+  });
+
+  it('Check defining Modules', () => {
+    expect(CategoryService).toBeDefined();
+  });
+
+  describe('GET /category', () => {
+    it('get all category', async () => {
+      // given
+      const userId = stubCategory[0].user.id;
+      const resBody = STUB_CATEGORY;
+      const categoryServSpy = jest
+        .spyOn(categoryService, 'readCategories')
+        .mockResolvedValue(resBody);
+
+      // when
+      const request = testRequest(app.getHttpServer()).get('/category');
+
+      // then
+      return request.expect(200).expect((res) => {
+        expect(categoryServSpy).toHaveBeenCalledWith(userId);
+        expect(res.body).toEqual(resBody);
+      });
+    });
+  });
+
+  describe('POST /category', () => {
+    it('expect success response with created category (without color)', async () => {
+      // given
+      const userId = stubCategory[0].user.id;
+      const categoryName = stubCategory[0].name;
+      const categoryId = stubCategory[0].id;
+      const color = stubCategory[0].color;
+      const reqBody = { name: categoryName };
+      const resBody = { name: categoryName, id: categoryId, color };
+      const categoryServSpy = jest
+        .spyOn(categoryService, 'createCategory')
+        .mockResolvedValue(resBody);
+
+      // when
+      const request = testRequest(app.getHttpServer())
+        .post('/category')
+        .send(reqBody);
+
+      // then
+      return request.expect(201).expect((res) => {
+        expect(categoryServSpy).toHaveBeenCalledWith({
+          categoryName,
+          userId,
+        });
+        expect(res.body).toEqual(resBody);
+      });
+    });
+  });
+
+  describe('POST /category', () => {
+    it('expect success response with created category (with color)', async () => {
+      // given
+      const userId = stubCategory[0].user.id;
+      const categoryName = stubCategory[0].name;
+      const categoryId = stubCategory[0].id;
+      const color = stubCategory[0].color;
+      const reqBody = { name: categoryName, color };
+      const resBody = { name: categoryName, id: categoryId, color };
+      const categoryServSpy = jest
+        .spyOn(categoryService, 'createCategory')
+        .mockResolvedValue(resBody);
+
+      // when
+      const request = testRequest(app.getHttpServer())
+        .post('/category')
+        .send(reqBody);
+
+      // then
+      return request.expect(201).expect((res) => {
+        expect(categoryServSpy).toHaveBeenCalledWith({
+          categoryName,
+          userId,
+        });
+        expect(res.body).toEqual(resBody);
+      });
+    });
+  });
+
+  describe('PUT /category/:categoryId', () => {
+    it('expect success response with updated category (without color)', async () => {
+      // given
+      const newCategoryName = 'new category name';
+      const categoryId = stubCategory[0].id;
+      const userId = stubCategory[0].user.id;
+      const color = stubCategory[0].color;
+      const reqBody = { name: newCategoryName };
+      const resBody = {
+        name: newCategoryName,
+        id: categoryId,
+        color,
+      };
+      const categoryServSpy = jest
+        .spyOn(categoryService, 'updateCategory')
+        .mockResolvedValue(resBody);
+
+      // when
+      const request = testRequest(app.getHttpServer())
+        .put(`/category/${categoryId}`)
+        .send(reqBody);
+
+      // then
+      return request.expect(200).expect((res) => {
+        expect(categoryServSpy).toHaveBeenCalledWith({
+          userId,
+          categoryId,
+          categoryName: newCategoryName,
+        });
+        expect(res.body).toEqual(resBody);
+      });
+    });
+  });
+
+  describe('PUT /category/:categoryId', () => {
+    it('expect success response with updated category (with color)', async () => {
+      // given
+      const newCategoryName = 'new category name';
+      const newColor = 0x333333;
+      const categoryId = stubCategory[0].id;
+      const userId = stubCategory[0].user.id;
+      const reqBody = { name: newCategoryName, color: newColor };
+      const resBody = {
+        name: newCategoryName,
+        id: categoryId,
+        color: newColor,
+      };
+      const categoryServSpy = jest
+        .spyOn(categoryService, 'updateCategory')
+        .mockResolvedValue(resBody);
+
+      // when
+      const request = testRequest(app.getHttpServer())
+        .put(`/category/${categoryId}`)
+        .send(reqBody);
+
+      // then
+      return request.expect(200).expect((res) => {
+        expect(categoryServSpy).toHaveBeenCalledWith({
+          userId,
+          categoryId,
+          color: newColor,
+          categoryName: newCategoryName,
+        });
+        expect(res.body).toEqual(resBody);
+      });
+    });
+  });
+
+  describe('DELETE /category/:categoryId', () => {
+    it('expect success response with deleted category', async () => {
+      // given
+      const userId = stubCategory[0].user.id;
+      const categoryId = stubCategory[0].id;
+      const categoryName = stubCategory[0].name;
+      const color = stubCategory[0].color;
+      const resBody = { id: categoryId, name: categoryName, color };
+      const categoryServSpy = jest
+        .spyOn(categoryService, 'deleteCategory')
+        .mockResolvedValue(resBody);
+
+      // when
+      const request = testRequest(app.getHttpServer()).delete(
+        `/category/${categoryId}`,
+      );
+
+      // then
+      return request.expect(200).expect((res) => {
+        expect(categoryServSpy).toHaveBeenCalledWith({ userId, categoryId });
+        expect(res.body).toEqual(resBody);
+      });
+    });
+  });
+});
