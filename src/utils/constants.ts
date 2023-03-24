488216b8e58965b733c0deee5be1b9c1b29825b6
seokho10007 <sukho1007@naver.com>
2023-03-24 14:41:34 +0900

    feat: env provider를 위한 constants 추가 (#29) - EnvironmentType: .env의 인자들의 이름을 정희 - ENV_PROVIDER: env provider의 이름을 정의

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,22 @@
-const PLANDAR_CUSTOM_REPSOITORY = 'PLANDAR_CUSTOM_REPSOITORY';
-
 enum TokenType {
   ACCESS = 'access',
   REFRESH = 'refresh',
 }
 
-export { TokenType, PLANDAR_CUSTOM_REPSOITORY };
+type EnvironmentType =
+  | 'JWT_SECRET_KEY'
+  | 'ACCESS_HEADER'
+  | 'REFRESH_HEADER'
+  | 'ACCESS_EXPIRES'
+  | 'REFRESH_EXPIRES'
+  | 'COOKIE_MAX_AGE';
+
+type AuthEnvironment = {
+  [key in EnvironmentType]: string;
+};
+
+const PLANDAR_CUSTOM_REPSOITORY = 'PLANDAR_CUSTOM_REPSOITORY';
+
+const ENV_PROVIDER = 'ENV_PROVIDER';
+
+export { TokenType, PLANDAR_CUSTOM_REPSOITORY, ENV_PROVIDER, AuthEnvironment };
