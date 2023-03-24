d36d855a1211eb3f851455897647d145587782f5
jintak0401 <jintak0401@naver.com>
2023-03-24 21:32:11 +0900

    feat: dto/category/args 적용하여 Category Service 함수 호출 (#32)

--- src/api/category/category.service.ts
+++ src/api/category/category.service.ts
@@ -1,24 +1,41 @@
 import { Injectable } from '@nestjs/common';
 
 import { CategoryRepository } from '@/api/category/category.repository';
+import {
+  CreateCategoryArgs,
+  DeleteCategoryArgs,
+  UpdateCategoryArgs,
+} from '@/dto/category';
 
 @Injectable()
 export class CategoryService {
   constructor(private readonly categoryRepo: CategoryRepository) {}
 
-  async readCategories(): Promise<any> {
+  async readCategory(userId: number): Promise<any> {
     return Promise.resolve();
   }
 
-  async createCategory(): Promise<any> {
+  async createCategory({
+    userId,
+    categoryName,
+    color,
+  }: CreateCategoryArgs): Promise<any> {
     return Promise.resolve();
   }
 
-  async updateCategory(): Promise<any> {
+  async updateCategory({
+    userId,
+    categoryName,
+    categoryId,
+    color,
+  }: UpdateCategoryArgs): Promise<any> {
     return Promise.resolve();
   }
 
-  async deleteCategory(): Promise<any> {
+  async deleteCategory({
+    userId,
+    categoryId,
+  }: DeleteCategoryArgs): Promise<any> {
     return Promise.resolve();
   }
 }
