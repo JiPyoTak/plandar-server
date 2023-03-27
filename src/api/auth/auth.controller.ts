f48c2e023f22a7e4c1e64c0310617078caf86308
seokho10007 <sukho1007@naver.com>
2023-03-27 17:05:01 +0900

    feat: auth controller oauth 관련 기능 구현 (#27) - kakao, google oauth 기능 및 success callback 생성

---
+++ src/api/auth/auth.controller.ts
@@ -0,0 +1,69 @@
+import { Controller, Get, Res, UseGuards } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
+
+import { User } from '@/decorators/user.decorator';
+import { User as UserEntity } from '@/entity/user.entity';
+import { TokenType } from '@/utils/constants';
+
+import { AuthService } from './auth.service';
+import { GoogleAuthGuard, KakaoAuthGuard } from './guards/oauth.guard';
+
+@Controller('auth')
+export class AuthController {
+  private readonly CLIENT_REDIRECT_URL: string;
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {
+    this.CLIENT_REDIRECT_URL = configService.get('CLIENT_REDIRECT_URL');
+  }
+
+  @Get('/')
+  async get() {
+    return 'auth';
+  }
+
+  @UseGuards(GoogleAuthGuard)
+  @Get('/google')
+  async googleLogin() {
+    return 'google auth login';
+  }
+
+  @UseGuards(GoogleAuthGuard)
+  @Get('/google/callback')
+  async googleLoginCallback(@User() user: UserEntity, @Res() res: Response) {
+    const [accessToken, refreshToken] = await this.authService.login(user);
+
+    this.authService.registerTokenInCookie(TokenType.ACCESS, accessToken, res);
+    this.authService.registerTokenInCookie(
+      TokenType.REFRESH,
+      refreshToken,
+      res,
+    );
+
+    return res.redirect(this.CLIENT_REDIRECT_URL);
+  }
+
+  @UseGuards(KakaoAuthGuard)
+  @Get('/kakao')
+  async kakaoLogin() {
+    return 'kakao auth login';
+  }
+
+  @UseGuards(KakaoAuthGuard)
+  @Get('/kakao/callback')
+  async kakaoLoginCallback(@User() user: UserEntity, @Res() res: Response) {
+    const [accessToken, refreshToken] = await this.authService.login(user);
+
+    this.authService.registerTokenInCookie(TokenType.ACCESS, accessToken, res);
+    this.authService.registerTokenInCookie(
+      TokenType.REFRESH,
+      refreshToken,
+      res,
+    );
+
+    return res.redirect(this.CLIENT_REDIRECT_URL);
+  }
+}
