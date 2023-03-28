ff3526e9f568ff6b989850f400661232904a36e3
seokho10007 <sukho1007@naver.com>
2023-03-28 17:01:00 +0900

    feat: auth controller 생성 (#28, #43) - refresh: bearer token을 이용한 access, refresh token 발급 - logout: 등록된 cookie 제거 - Public decorator를 통해 public route 설정

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Response } from 'express';
 
+import { Public } from '@/decorators/skip-auth.decorator';
 import { User } from '@/decorators/user.decorator';
 import { User as UserEntity } from '@/entity/user.entity';
-import { TokenType } from '@/types';
+import { JwtTokenType } from '@/types';
 
 import { AuthService } from './auth.service';
 import { GoogleAuthGuard, KakaoAuthGuard } from './guards/oauth.guard';
@@ -23,42 +24,66 @@ export class AuthController {
   async loginCallback(user: UserEntity, res: Response) {
     const [accessToken, refreshToken] = await this.authService.login(user);
 
-    this.authService.registerTokenInCookie(TokenType.ACCESS, accessToken, res);
     this.authService.registerTokenInCookie(
-      TokenType.REFRESH,
+      JwtTokenType.ACCESS,
+      accessToken,
+      res,
+    );
+    this.authService.registerTokenInCookie(
+      JwtTokenType.REFRESH,
       refreshToken,
       res,
     );
-
-    res.redirect(this.CLIENT_REDIRECT_URL);
   }
 
+  @Public()
   @Get('/')
   async get() {
     return 'auth';
   }
 
+  @Public()
   @UseGuards(GoogleAuthGuard)
   @Get('/google')
   async googleLogin() {
     return 'google auth login';
   }
 
+  @Public()
   @UseGuards(KakaoAuthGuard)
   @Get('/kakao')
   async kakaoLogin() {
     return 'kakao auth login';
   }
 
+  @Public()
   @UseGuards(GoogleAuthGuard)
   @Get('/google/callback')
   async googleLoginCallback(@User() user: UserEntity, @Res() res: Response) {
-    return this.loginCallback(user, res);
+    await this.loginCallback(user, res);
+
+    return res.redirect(this.CLIENT_REDIRECT_URL);
   }
 
+  @Public()
   @UseGuards(KakaoAuthGuard)
   @Get('/kakao/callback')
   async kakaoLoginCallback(@User() user: UserEntity, @Res() res: Response) {
-    return this.loginCallback(user, res);
+    await this.loginCallback(user, res);
+
+    return res.redirect(this.CLIENT_REDIRECT_URL);
+  }
+
+  @Post('/refresh')
+  async refresh(
+    @User() user: UserEntity,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    return await this.loginCallback(user, res);
+  }
+
+  @Post('/logout')
+  async logout(@Res({ passthrough: true }) res: Response) {
+    return await this.authService.clearCookie(res);
   }
 }
