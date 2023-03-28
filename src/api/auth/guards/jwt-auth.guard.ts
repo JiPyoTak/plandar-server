a4cffd1856037a62e911a3404b52f24c6e461d21
seokho10007 <sukho1007@naver.com>
2023-03-28 16:49:34 +0900

    feat: JwtAuthGuard 생성 (#28) - Public key를 검사하여 Guard를 실행할지 결정하는 로직 추가 - 완료시 복호화된 payload에서 user id 추출하여 req.user에 저장

---
+++ src/api/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,36 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+
+import { IS_PUBLIC_KEY } from '@/decorators/skip-auth.decorator';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+
+  handleRequest(err: unknown, user: any, info: any) {
+    if (err || !user) {
+      throw err || new UnauthorizedException(info.message);
+    }
+    return user;
+  }
+}
