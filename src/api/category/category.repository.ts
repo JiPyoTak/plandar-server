0748b0ce64d25f8bc48e66da6229320f59043788
jintak0401 <jintak0401@naver.com>
2023-03-28 16:22:39 +0900

    feat: Category Repository 구현 (#34)

--- src/api/category/category.repository.ts
+++ src/api/category/category.repository.ts
@@ -1,31 +1,96 @@
 import { Repository } from 'typeorm';
 
 import { CustomRepository } from '@/common/customRepository.decorator';
+import {
+  CategoryResDto,
+  CreateCategoryArgs,
+  DeleteCategoryArgs,
+  UpdateCategoryArgs,
+} from '@/dto/category';
 import { Category } from '@/entity/category.entity';
+
+const SELECT = {
+  id: true,
+  name: true,
+  color: true,
+};
 
 @CustomRepository(Category)
 export class CategoryRepository extends Repository<Category> {
-  async findCategoryById(): Promise<any> {
-    return Promise.resolve();
+  async findCategoryById({
+    userId,
+    categoryId,
+  }: {
+    userId: number;
+    categoryId: number;
+  }): Promise<CategoryResDto> {
+    return this.findOne({
+      where: {
+        id: categoryId,
+        user: { id: userId },
+      },
+      select: SELECT,
+    });
   }
 
-  async findCategoryByName(): Promise<any> {
-    return Promise.resolve();
+  async findCategoryByName({
+    userId,
+    categoryName,
+  }: {
+    userId: number;
+    categoryName: string;
+  }): Promise<CategoryResDto> {
+    return this.findOne({
+      where: {
+        name: categoryName,
+        user: { id: userId },
+      },
+      select: SELECT,
+    });
   }
 
-  async readCategory(): Promise<any> {
-    return Promise.resolve();
+  async readCategory(userId: number): Promise<CategoryResDto[]> {
+    return this.find({
+      where: {
+        user: { id: userId },
+      },
+      select: SELECT,
+    });
   }
 
-  async createCategory(): Promise<any> {
-    return Promise.resolve();
+  async createCategory({
+    userId,
+    categoryName,
+    color,
+  }: CreateCategoryArgs): Promise<CategoryResDto> {
+    const {
+      identifiers: [{ id }],
+    } = await this.insert({
+      name: categoryName,
+      color,
+      user: { id: userId },
+    });
+    return this.findCategoryById({ userId, categoryId: id });
   }
 
-  async updateCategory(): Promise<any> {
-    return Promise.resolve();
+  async updateCategory({
+    userId,
+    categoryId,
+    categoryName,
+    color,
+  }: UpdateCategoryArgs): Promise<CategoryResDto> {
+    await this.update({ id: categoryId }, { name: categoryName, color });
+    return this.findCategoryById({ userId, categoryId });
   }
 
-  async deleteCategory(): Promise<any> {
-    return Promise.resolve();
+  async deleteCategory({
+    categoryId,
+    userId,
+  }: DeleteCategoryArgs): Promise<boolean> {
+    const { affected } = await this.delete({
+      id: categoryId,
+      user: { id: userId },
+    });
+    return !!affected;
   }
 }
