1bf4722b87bef6ff7b589b6b58a00822cfdd22b3
jintak0401 <jintak0401@naver.com>
2023-03-28 15:46:19 +0900

    feat: Category Repository 각 함수 임시 구현 (#33)

--- src/api/category/category.repository.ts
+++ src/api/category/category.repository.ts
@@ -4,4 +4,28 @@ import { CustomRepository } from '@/common/customRepository.decorator';
 import { Category } from '@/entity/category.entity';
 
 @CustomRepository(Category)
-export class CategoryRepository extends Repository<Category> {}
+export class CategoryRepository extends Repository<Category> {
+  async findCategoryById(): Promise<any> {
+    return Promise.resolve();
+  }
+
+  async findCategoryByName(): Promise<any> {
+    return Promise.resolve();
+  }
+
+  async readCategory(): Promise<any> {
+    return Promise.resolve();
+  }
+
+  async createCategory(): Promise<any> {
+    return Promise.resolve();
+  }
+
+  async updateCategory(): Promise<any> {
+    return Promise.resolve();
+  }
+
+  async deleteCategory(): Promise<any> {
+    return Promise.resolve();
+  }
+}
