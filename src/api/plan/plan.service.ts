0a1957ee26b326d7ef3e1b96f159ccf2e61b2ef8
pyo-sh <pygosky@gmail.com>
2023-03-31 15:49:51 +0900

    feat: Plan Controller 실행을 위해 Plan Service 틀 및 타입 작성 (#38)

--- src/api/plan/plan.service.ts
+++ src/api/plan/plan.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
+import { PlanEntity } from '@/entities';
+
 import { PlanRepository } from './plan.repository';
 
 @Injectable()
 export class PlanService {
-  constructor(private readonly planRepo: PlanRepository) {}
+  constructor(private readonly planRepository: PlanRepository) {}
+
+  async getPlans(data): Promise<PlanEntity[]> {
+    return [];
+  }
+
+  async createPlan(data): Promise<PlanEntity> {
+    return {} as PlanEntity;
+  }
+
+  async updatePlan(data): Promise<PlanEntity> {
+    return {} as PlanEntity;
+  }
+
+  async deletePlan(data): Promise<PlanEntity> {
+    return {} as PlanEntity;
+  }
 }
