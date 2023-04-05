7dd8e743f573d4aa1e3f308172cf17d7f3063dbb
pyo-sh <pygosky@gmail.com>
2023-04-05 15:53:16 +0900

    test: PlanService 에서 예상되는 Injection Class를 미리 선언해서 모듈 테스팅하기 (#41)

--- test/api/plan/plan.service.spec.ts
+++ test/api/plan/plan.service.spec.ts
@@ -1,22 +1,30 @@
+import { CategoryService } from '@/api/category/category.service';
 import { PlanRepository } from '@/api/plan/plan.repository';
 import { PlanService } from '@/api/plan/plan.service';
+import { TagService } from '@/api/tag/tag.service';
 import createTestingModule from 'test/utils/create-testing-module';
 
 describe('PlanService', () => {
   let planRepository: PlanRepository;
   let planService: PlanService;
+  let categoryService: CategoryService;
+  let tagService: TagService;
 
   beforeEach(async () => {
     const moduleRef = await createTestingModule({
-      providers: [PlanService],
+      providers: [PlanService, CategoryService, TagService],
     });
 
     planRepository = moduleRef.get<PlanRepository>(PlanRepository);
+    categoryService = moduleRef.get<CategoryService>(CategoryService);
+    tagService = moduleRef.get<TagService>(TagService);
     planService = moduleRef.get<PlanService>(PlanService);
   });
 
   it('Check defining Modules', () => {
     expect(planRepository).toBeDefined();
+    expect(categoryService).toBeDefined();
+    expect(tagService).toBeDefined();
     expect(planService).toBeDefined();
   });
 });
