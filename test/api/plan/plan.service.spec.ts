086df7a2749a81f2ab9b12a2e07f120148e539bb
pyo-sh <pygosky@gmail.com>
2023-04-05 15:54:47 +0900

    test: Plan을 user가 소유하고 있는지에 대한 값을 비교하는 로직 테스팅 - 성공 (#41)

--- test/api/plan/plan.service.spec.ts
+++ test/api/plan/plan.service.spec.ts
@@ -2,6 +2,8 @@ import { CategoryService } from '@/api/category/category.service';
 import { PlanRepository } from '@/api/plan/plan.repository';
 import { PlanService } from '@/api/plan/plan.service';
 import { TagService } from '@/api/tag/tag.service';
+import { PLAN_STUB } from 'test/api/plan/stub';
+import { USER_STUB } from 'test/api/user/stub';
 import createTestingModule from 'test/utils/create-testing-module';
 
 describe('PlanService', () => {
@@ -26,5 +28,22 @@ describe('PlanService', () => {
     expect(categoryService).toBeDefined();
     expect(tagService).toBeDefined();
     expect(planService).toBeDefined();
+  });
+
+  describe('checkUserOwnPlan', () => {
+    it(`should pass - userId & plan's userId are same`, async () => {
+      const args = {
+        userId: USER_STUB.id,
+        planId: PLAN_STUB.id,
+      };
+      const planRepoSpy = jest
+        .spyOn(planRepository, 'findOnlyUserId')
+        .mockResolvedValue(args.userId);
+
+      await planService.checkUserOwnPlan(args);
+
+      expect(planRepoSpy).toHaveBeenCalledTimes(1);
+      expect(planRepoSpy).toHaveBeenCalledWith(args.planId);
+    });
   });
 });
