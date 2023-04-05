725476c64e7c093356498d1fc8c1bdfe51170ef6
pyo-sh <pygosky@gmail.com>
2023-04-05 16:07:06 +0900

    feat: PlanRepository - id로 Plan을 찾을 수 있는 findPlanById 작성 (#40)

--- src/api/plan/plan.repository.ts
+++ src/api/plan/plan.repository.ts
@@ -1,7 +1,17 @@
 import { Repository } from 'typeorm';
 
 import { CustomRepository } from '@/common/decorators';
+import { PlanResDto } from '@/dto/plan';
 import { PlanEntity } from '@/entities';
+import { PLAN_SELECT } from '@/utils/constants';
 
 @CustomRepository(PlanEntity)
-export class PlanRepository extends Repository<PlanEntity> {}
+export class PlanRepository extends Repository<PlanEntity> {
+  async findPlanById(id: number): Promise<PlanResDto> {
+    return await this.findOne({
+      where: { id },
+      select: PLAN_SELECT,
+      relations: { category: false, tags: true },
+    });
+  }
+}
