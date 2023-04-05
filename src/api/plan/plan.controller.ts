828843a41abf4d20f9ec8ca8bbe32f555b382496
pyo-sh <pygosky@gmail.com>
2023-04-05 15:48:53 +0900

    feat: Controller에서 Swagger API 에 대한 example & response 값을 지정해주기 (#38)

--- src/api/plan/plan.controller.ts
+++ src/api/plan/plan.controller.ts
@@ -10,10 +10,20 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiQuery,
+} from '@nestjs/swagger';
 
 import { User } from '@/common/decorators/user.decorator';
 import { ParseDatePipe } from '@/common/pipes';
-import { PlanCreateReqDto, PlanUpdateReqDto } from '@/dto/plan';
+import { PlanCreateReqDto, PlanResDto, PlanUpdateReqDto } from '@/dto/plan';
 import { UserEntity } from '@/entities';
 
 import { PlanService } from './plan.service';
@@ -22,6 +32,29 @@ import { PlanService } from './plan.service';
 export class PlanController {
   constructor(private readonly planService: PlanService) {}
 
+  @ApiOperation({
+    summary: '원하는 날짜의 일정 조회',
+  })
+  @ApiQuery({
+    name: 'timemin',
+    type: Date,
+    example: '2023-04-01T00:00:00.000Z',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'timemax',
+    type: Date,
+    example: '2023-05-01T00:00:00.000Z',
+    required: true,
+  })
+  @ApiOkResponse({
+    description: '일정 조회 성공',
+    type: PlanResDto,
+    isArray: true,
+  })
+  @ApiBadRequestResponse({
+    description: 'timemin query 값이 timemax query 값보다 이후일 때',
+  })
   @Get('/')
   async getPlans(
     @Query('timemin', ParseDatePipe) timeMin: Date,
@@ -37,6 +70,16 @@ export class PlanController {
     return this.planService.getPlans({ timeMin, timeMax, userId: user.id });
   }
 
+  @ApiOperation({
+    summary: '새로운 일정 추가',
+  })
+  @ApiCreatedResponse({
+    description: '일정 생성 성공',
+    type: PlanResDto,
+  })
+  @ApiConflictResponse({
+    description: '연결하고자 하는 카테고리 아이디가 존재하지 않는 경우',
+  })
   @Post('/')
   async createPlan(
     @Body() createPlanReqDto: PlanCreateReqDto,
@@ -48,6 +91,23 @@ export class PlanController {
     });
   }
 
+  @ApiOperation({
+    summary: '일정 수정',
+  })
+  @ApiOkResponse({
+    description: '일정 수정 성공',
+    type: PlanResDto,
+  })
+  @ApiBadRequestResponse({
+    description: '바꾸려는 일정 값이(Body 혹은 planId 값) 존재하지 않을 때',
+  })
+  @ApiConflictResponse({
+    description:
+      '일정 아이디가 존재하지 않거나 카테고리 아이디가 존재하지 않는 경우',
+  })
+  @ApiForbiddenResponse({
+    description: '일정 혹은 카테고리가 유저가 소유한 것이 아닐 경우',
+  })
   @Put('/:planId')
   async updatePlan(
     @Param('planId', ParseIntPipe) planId: number,
@@ -66,6 +126,22 @@ export class PlanController {
     });
   }
 
+  @ApiOperation({
+    summary: '일정 삭제',
+  })
+  @ApiOkResponse({
+    description: '일정 삭제 성공',
+    type: PlanResDto,
+  })
+  @ApiConflictResponse({
+    description: '일정 아이디가 존재하지 않을 경우',
+  })
+  @ApiForbiddenResponse({
+    description: '일정이 유저가 소유한 것이 아닐 경우',
+  })
+  @ApiInternalServerErrorResponse({
+    description: '일정 삭제가 데이터베이스 오류로 실패된 경우',
+  })
   @Delete('/:planId')
   async deletePlan(
     @Param('planId', ParseIntPipe) planId: number,
